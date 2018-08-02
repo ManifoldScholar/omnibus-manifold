da5108d30305c313338b14684a4e5de44d230b71
Alexa Grey <alexa@castironcoding.com>
2018-08-02 13:43:38 -0700

    [C] Use manifold-hosted sources for JRE & IMagick

--- config/software/jre.rb
+++ config/software/jre.rb
@@ -39,21 +39,18 @@ whitelist_file "jre/plugin"
 whitelist_file "jre/bin/appletviewer"
 
 license_warning = "By including the JRE, you accept the terms of the Oracle Binary Code License Agreement for the Java SE Platform Products and JavaFX, which can be found at http://www.oracle.com/technetwork/java/javase/terms/license/index.html"
-license_cookie = "gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie"
 
 version "8u181" do
   if mac_os_x?
     # https://www.oracle.com/webfolder/s/digest/8u121checksum.html
-    source url: "http://download.oracle.com/otn-pub/java/jdk/8u181-b13/96a7b8442fe848ef90c96a2fad6ed6d1/jre-8u181-macosx-x64.tar.gz",
+    source url: "https://storage.googleapis.com/manifold-assets/omnibus/jre/jre-8u181-macosx-x64.tar.gz",
            sha256: "7d2e6275f77bfa0f0eaca03c6d48e27fa822abcae056d30293007978957ce3fc",
-           cookie: license_cookie,
            warning: license_warning,
            unsafe:  true
     relative_path "jre1.8.0_181.jre/Contents/Home"
   else
-    source url: "http://download.oracle.com/otn-pub/java/jdk/8u181-b13/96a7b8442fe848ef90c96a2fad6ed6d1/jre-8u181-linux-x64.tar.gz",
+    source url: "https://storage.googleapis.com/manifold-assets/omnibus/jre/jre-8u181-linux-x64.tar.gz",
            sha256: "0b26c7fcfad20029e6e0989e678efcd4a81f0fe502a478b4972215533867de1b",
-           cookie: license_cookie,
            warning: license_warning,
            unsafe:  true
     relative_path "jre1.8.0_181"
