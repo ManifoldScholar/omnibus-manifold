329440402714e9cb09707c44ac757349dc6f4694
Zach Davis <zach@castironcoding.com>
2018-08-29 14:02:13 -0700

    [B] Remove omnibus-toolchain tar on ubuntu 18

--- lib/scripts/provision-ubuntu-18.sh
+++ lib/scripts/provision-ubuntu-18.sh
@@ -2,4 +2,9 @@
 
 export DEBIAN_FRONTEND=noninteractive
 
+# https://github.com/chef/omnibus-toolchain/issues/73
+rm /opt/omnibus-toolchain/bin/tar
+rm /opt/omnibus-toolchain/bin/gtar
+rm /opt/omnibus-toolchain/embedded/bin/tar
+
 apt-get update
