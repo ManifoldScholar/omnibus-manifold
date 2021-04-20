2900f97a104b64e536cd7fd9e0970a71d9b44706
Zach Davis <zach@castironcoding.com>
2021-04-20 12:30:45 -0700

    [C] Remove workaround for upstream issue

--- lib/scripts/provision-ubuntu-18.sh
+++ lib/scripts/provision-ubuntu-18.sh
@@ -2,9 +2,4 @@
 
 export DEBIAN_FRONTEND=noninteractive
 
-# https://github.com/chef/omnibus-toolchain/issues/73
-rm /opt/omnibus-toolchain/bin/tar
-rm /opt/omnibus-toolchain/bin/gtar
-rm /opt/omnibus-toolchain/embedded/bin/tar
-
 apt-get update
